2a482553bae92e2a7a0b0568980de6beb9a05056
raghunatht-twi <raghunath.t@thoughtworks.com>
2025-01-26 08:37:00 +0530

    Regression Examples

---
+++ linearRegression.py
@@ -0,0 +1,13 @@
+# Linear Regression Examples
+# linearRegression.py
+# Regression_py
+#
+# Created by Raghunath Tripasuri on 26/01/25.
+
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+import math;
+
+print("Linear Regression Examples");
+
