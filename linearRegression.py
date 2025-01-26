a0826156864df70c8bc656b7bb45e3a4ed0350cb
raghunatht-twi <raghunath.t@thoughtworks.com>
2025-01-26 09:44:27 +0530

    reading data, committing data

--- linearRegression.py
+++ linearRegression.py
@@ -11,3 +11,13 @@ import math;
 
 print("Linear Regression Examples");
 
+#Set the print options for pandas
+np.set_printoptions(precision=4, linewidth=100);
+
+#Load the data for MBA Salary from file
+mba_salary_df = pd.read_csv("/Users/raghunatht/Documents/Programming/Python/Regression_py/Data/MBA Salary.csv");
+print(mba_salary_df.head(10));
+print(mba_salary_df.info());
+
+
+
